c01f460b35946c3e643f50bb6555eb0ae982dae1
musham27 <musham.vamshi@gmail.com>
2024-12-18 17:46:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
+  ami                    = "ami-036841078a4b68e14"
+  instance_type          = "t2.micro" 
+  key_name               = "ohio"
+  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
+  tags = {
+    Name = "u21.local"
+  }
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo hostnamectl set-hostname U21.local
+  # netdata_conf="/etc/netdata/netdata.conf"
+  # Path to netdata.conf
+  # actual_ip=0.0.0.0
+  # Use sed to replace the IP address in netdata.conf
+  # sudo sed -i "s/bind socket to IP = .*$/bind socket to IP = $actual_ip/" "$netdata_conf"
+EOF
+
+}
+
+resource "aws_instance" "frontend" { #playbook.yaml NGINX
+  ami                    = "ami-088d38b423bff245f"
+  instance_type          = "t2.micro"
+  key_name               = "ohio"
+  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
+  tags = {
+    Name = "c8.local"
+  }
+  user_data = <<-EOF
+  #!/bin/bash
+  # New hostname and IP address
+  sudo hostnamectl set-hostname u21.local
+  hostname=$(hostname)
+  public_ip="$(curl -s https://api64.ipify.org?format=json | jq -r .ip)"
+
+  # Path to /etc/hosts
+  echo "${aws_instance.backend.public_ip} $hostname" | sudo tee -a /etc/hosts
+
+EOF
+depends_on = [aws_instance.backend]
+}
+
+resource "local_file" "inventory" {
+  filename = "./inventory.yaml"
+  content  = <<EOF
+[frontend]
+${aws_instance.frontend.public_ip}
+[backend]
+${aws_instance.backend.public_ip}
+EOF
+}
+
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
+}
